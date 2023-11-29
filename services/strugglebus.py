a002d1237cf19e8400675af8657c30ef6779fd36
specbug <rishit7.rv@gmail.com>
2023-11-29 23:16:59 +0530

    swift init

--- services/strugglebus.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-
-from models.strugglebus import Event
-
-
-def get_random_action_state(event: Event) -> bool:
-    probability = float(os.getenv(event))
-    return random.random() < probability
