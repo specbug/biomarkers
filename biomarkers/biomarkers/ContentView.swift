2a20058d58563d414b5f024ec528857fa259fb1b
specbug <rishit7.rv@gmail.com>
2023-12-01 21:02:16 +0530

    metrics UI

---
+++ biomarkers/biomarkers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  biomarkers
+//
+//  Created by Rishit Vora on 29/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ biomarkers/biomarkers/biomarkersApp.swift
@@ -0,0 +1,17 @@
+//
+//  biomarkersApp.swift
+//  biomarkers
+//
+//  Created by Rishit Vora on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct biomarkersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
